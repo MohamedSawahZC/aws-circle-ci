8767f9a7afb95d1119ad8e993485e64a22c89b93
unknown <sawah810@gmail.com>
2022-06-28 12:50:51 +0200

    handle deploy bin

--- server/bin/deploy.sh
+++ server/bin/deploy.sh
@@ -2,13 +2,13 @@
 echo "*******__ Udagram Server API  __*******"
 
 echo "*******__ 1.Create a repository with the eb init command.....👨🏻‍💻🤌🏻  __*******"
-eb init udagram-api --platform node.js --region us-east-1
+eb init aws-circle-ci-api --platform node.js --region us-east-1
 
 echo "*******__ 2.Sets the specified environment....👨🏻‍💻🤌🏻  __*******"
-eb use udagram-api-dev
+eb use aws-circle-ci-api-dev
 
 echo "*******__ 3.Deploy the changes....👨🏻‍💻🤌🏻  __*******"
-eb deploy udagram-api-dev
+eb deploy aws-circle-ci-api-dev
 
 echo "*******__ 4.Set Env Variables....👨🏻‍💻🤌🏻  __*******"
-eb setenv AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_REGION DB_PORT=$DB_PORT PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME AWS_S3_ENDPOINT=$AWS_S3_ENDPOINT
+eb setenv AWS_BUCKET=s3://sawahe AWS_REGION=us-east-1 DB_PORT=5432 PORT=3000 POSTGRES_DB=postgres POSTGRES_HOST=database-1.cra9lcbtikon.us-east-1.rds.amazonaws.com POSTGRES_PASSWORD=postgres POSTGRES_USERNAME=postgres AWS_S3_ENDPOINT=aws-circle-ci-api-dev.eba-qcfvpf4b.us-east-1.elasticbeanstalk.com
